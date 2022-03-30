de955671ef913947f07adaec4a80e73d43321b79
David Williamson <david.williamson@varilink.co.uk>
2022-03-30 10:53:58 +0100

    SOME SMALL TWEAKS
    As follows:
    
     - Removed theme volume mapping so that project's can set it themselvses
       in their own Docker Compose file. This allows project's to dicate the
       folder name to match the theme that they are using.
    
     - Changed the build path for the wp-cli serice to reflect that this
       tool is used by project's as a submodule in a nested submodule
       folder.
    
     - Pass the COMPOSE_PROJECT_NAME through to the wp-cli service so that
       its value can be referenced in the entrypoint script.
    
     - Removed the commented out reference to an external proxy network as
       useage has confirmed that we're definitely not doing it that way now.
    
     - Expanded the entrypoint script a little. This is a work in progress.
       Probably the README will need to be updated to reflect its
       functionality once it is stable.
    
    Committing as I'm now going to use this tool for the Varilink Website
    project, which also uses WordPress and so I need to check it out there.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,34 +2,84 @@
 
 set -e
 
+if [ "$1" = "_" ]; then
+
+  echo "Helpers available"
+  PS3='Helper?'
+  select HELPER in                                                             \
+    _create-admin-user                                                         \
+    _remove-contact-form-recaptcha-integration                                 \
+    _restore-from-backup
+  do
+    if [[ -n $HELPER ]]; then
+      set -- "$HELPER"
+      break
+    else
+      echo "Invalid selection, enter number for the desired helper in the list"
+    fi
+  done
+
+fi
+
 case $1 in
 
-  _restore)
+  _create-admin-user)
 
-    # Find the archive of the website's home folder.
-    echo "Restoring wp-content folders"
+    # Create an admin user for use within the container
+
+    # It doesn't matter that the credentials are not secure since we're only
+    # going to use this user on the developer desktop.
+    wp user create admin admin@localhost.localdomain                           \
+      --role=administrator --user_pass=password
+
+    ;;
+
+  _remove-contact-form-recaptcha-integration)
+
+    # Remove Contact Form 7 integration with reCAPTCHA
+
+    wp option patch delete wpcf7 recaptcha
+
+    ;;
+
+  _restore-from-backup)
+
+    echo "Restoring the WordPress installation from the backup to the container"
+    # Find the archive file of website's home folder
     archive=$(find /backup -name "*.tar.gz")
-    tar --extract --directory=/tmp --file=$archive
-    wp_content=$(find /tmp -name wp-content)
-    for DIR in plugins themes uploads
-    do
-      rm -rf /var/www/html/wp-content/$DIR
-      mv $wp_content/$DIR /var/www/html/wp-content/$DIR
-    done
-    rm -rf /tmp/*
+    echo "Archive file found at $archive"
+    # Get the full path of the wp_config file from the archive
+    wp_config=$(tar --list --file=$archive --wildcards "*/wp-config.php")
+    echo "wp-config.php found at $wp_config"
+    # Extract all the forward slashes from the full path of the wp_config file
+    slashes="${wp_config//[^\/]}"
+    # Count the components to strip from file names on extraction
+    components="${#slashes}"
+    # Extract from the archive to the WordPress home in the container
+    tar                                                                        \
+      --extract                                                                \
+      --file=$archive                                                          \
+      --directory=/var/www/html                                                \
+      --strip-components=$components                                           \
+      --exclude=$wp_config
+    echo "Extract from the archive completed"
 
-    echo "Search replace of site URL"
+    echo                                                                       \
+      "Change this site's URLs in the WordPress database to match the container"
     IFS=':'
     read -a environments <<< "$ENVIRONMENTS"
     for environment in "${environments[@]}"
     do
-      wp search-replace                                                        \
+      echo                                                                     \
+        "Replace https://$environment.$DOMAIN with http://$COMPOSE_PROJECT_NAME"
+      wp search-replace --report-changed-only                                  \
         https://$environment.$DOMAIN http://$COMPOSE_PROJECT_NAME
     done
 
     ;;
 
   *)
+
     wp $@
     ;;
 
