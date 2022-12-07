269886d4f37a85300e1e7d351cefde3ee20dd8e5
David Williamson <david.williamson@varilink.co.uk>
2022-12-07 22:52:39 +0000

    VARIOUS CHANGES
    Changes as follows:
    
     - Expand functionality of heplers implemented via docker-entrypoint.sh.
    
     - Implement volume for WordPress files.
    
     - Specify host user and group in seprately providded env file.
    
     - Map to posts folder for processing migrations.
    
     - Updated README to reflect all these changes.

--- README.md
+++ README.md
@@ -9,25 +9,47 @@ Provides Docker Compose services that can be used to run any WordPress based web
 ## How to use this tool in a project
 
 1. Add this repository as a submodule of a project that uses it. To make the project's WordPress website available in the web browser on that desktop, that project must also use my [proxy tool](https://github.com/varilink/tools-proxy).
+
 2. Set the following Docker Compose environment variables in the project:
    - COMPOSE_PROJECT_NAME
    - WORDPRESS_TAG
-   - MARIADB-TAG
+   - MARIADB_TAG
+
+
 3. Add any additional Docker Compose files to your project that you require in order to extend the services defined in this repository; for example in order to define additional, project specific environment variables.
+
 4. Be sure to apply all the required Docker Compose files in the right order, which can be done using the COMPOSE_FILE environment variable for convenience.
 
 An good example of this tool used in a project is can be found in my own [FoBV - Docker](https://github.com/varilink/fobv-docker) repository.
 
-Note that you must include a Docker Compose file in the project's root folder **first** in the COMPOSE_FILE variable since this then sets the root folder for any relative paths in all the subsequent Docker Compose files - see [this issue on GitHub](https://github.com/docker/compose/issues/3874), which identifies the workaround of using an empty Docker Compose file in the project's root folder where there isn't already one in there that can be referenced first.
+Note that you must include a Docker Compose file in the project's root folder **first** in the `COMPOSE_FILE` variable since this then sets the root folder for any relative paths in all the subsequent Docker Compose files - see [this issue on GitHub](https://github.com/docker/compose/issues/3874), which identifies the workaround of using an empty Docker Compose file in the project's root folder where there isn't already one in there that can be referenced first.
 
 ## Starting a project from scratch
-When starting work on a project for the first time, with no existing project containers or volumes in place, simply bringing the project's wordpress service up via `docker-compose up wordpress` will enable you to run the WordPress installation script for your development website at `http://${COMPOSE_PROJECT_NAME}` on your desktop.
+When starting work on a project for the first time, with no existing project containers or volumes in place, simply bringing the project's `wordpress` service up via `docker-compose up wordpress` will enable you to run the WordPress installation script for your development website at `http://${COMPOSE_PROJECT_NAME}` on your desktop.
 
-Note that the WordPress installation folder is stored in a volume named wordpress, which will be created with the name `${COMPOSE_PROJECT_NAME}_wordpress`. This is because it is shared by both the wordpress and wp-cli Docker Compose services. Be alert that this means it must be explicitly removed if that's required. It can't be cleared by simply removing a container.
+In this mode it's probable that you won't need to provide any project overrides for the Docker Compose services provided by this tool. As noted above however, the Docker Compose file listed first in the `COMPOSE_FILE` must be a project file in the project's root folder. As highlighted in the GitHub issue referenced above, this can be achieved using a Docker Compose file that is empty, save for the Docker Compose file version number; for example:
+
+```yaml
+version: "3.6"
+```
+
+The WordPress site is made available at `http://${COMPOSE_PROJECT_NAME}` by my [proxy tool](https://github.com/varilink/tools-proxy) **only** if you make an entry in your hosts file as explained under [DNS / Host name lookup](https://github.com/varilink/tools-proxy#dns--host-name-lookup) in the README for my [proxy tool](https://github.com/varilink/tools-proxy).
+
+The WordPress installation folder is stored in a volume named wordpress, which will be created with the name `${COMPOSE_PROJECT_NAME}_wordpress`. This is because it is shared by both the wordpress and wp-cli Docker Compose services. Be alert that this means it must be explicitly removed if that's required. It can't be cleared by simply removing a container.
 
 ## Restoring a project from backup
-You can restore a WordPress website from backup to work on updates to it locally. To do this your project must contain a folder `/backup` that has in it:
-1. A dump of the website's database with one of the extensions listed under "Initialzing a fresh instance" on the [MariaDB Docker Hub page](https://hub.docker.com/_/mariadb).
+You can restore a WordPress website from backup to work on updates to it locally. To do this you must provide a fuller, project Docker Compose file that extends the `db` and `wp-cli` services with volumes as follows:
+
+- For the `db` service, a volume that maps a folder containing a data dump file to restore from, mapped to the folder `/docker-entrypoint-initdb.d` in the container - see under "Initialzing a fresh instance" on the [MariaDB Docker Hub page](https://hub.docker.com/_/mariadb).
+
+- For the `wp-cli` service, a volume that maps a folder containing a compressed, tar archive (extension `.tar.gz`) of the website's WordPress installation folder.
+
+
+
+ To do this your project must contain a folder `/backup` that has in it:
+
+1. A dump of the website's database with one of the extensions listed
+
 2. A compressed, tar archive (extension `.tar.gz`) of the website's WordPress files.
 
 The restore of the database takes a little while. So, it's best to bring your project's db service up first and wait for this to happen. If you just bring the wordpress service up and let it start the db service as a dependent service before the database restore has happened then the wordpress service will be unable to connect to the database as the db service is unavailable while the restore is in progress. By the time the db service becomes available the wordpress service will have timed out. Once the database has been restored on repeat occasions that you're bringing services up you won't have to worry about this.
@@ -41,6 +63,11 @@ The wp-cli access provides the [WP-CLI command line interface for WordPress](htt
 The service also wraps the WP CLI with helper shortcuts. These are invoked by passing it commands that are prefixed with an underscore to avoid any clash with a built-in WP CLI command. At the moment there is just one of these, `_restore`, which is invoked using the command `docker-compose run --rm wp-cli _restore`. Others may be added in due course.
 
 Here is guidance on how to use the defined helpers:
+
+### _correct_site_url
+
+*Before this is done you will see a redirect to the site that the backup was taken from*
+
 
 ### _restore
 
--- wp-cli/docker-entrypoint.sh
+++ wp-cli/docker-entrypoint.sh
@@ -1,23 +1,39 @@
 #!/usr/bin/env bash
 
+# Wrapper script to WP-CLI that provides several shortcut "helper scripts" as
+# well as the native fuctionality of WP-CLI itself.
+
 set -e
 
-if [ "$1" = "_" ]; then
+if [ "$1" = '_' ]
+  then
 
-  echo "Helpers available"
-  PS3='Helper?'
-  select HELPER in                                                             \
-    _create-admin-user                                                         \
-    _remove-contact-form-recaptcha-integration                                 \
-    _restore-from-backup
-  do
-    if [[ -n $HELPER ]]; then
-      set -- "$HELPER"
-      break
-    else
-      echo "Invalid selection, enter number for the desired helper in the list"
-    fi
-  done
+    # I've been called with the single argument '_', which means "Show me the
+    # available helpers so that I may select one of them."
+
+    echo 'Helpers available'
+    PS3='Helper? '
+    select HELPER in                                                           \
+      _create-admin-user                                                       \
+      _correct_site_url                                                        \
+      _export_post                                                             \
+      _import_post                                                             \
+      _install_importer                                                        \
+      _remove-contact-form-recaptcha-integration                               \
+      _restore-from-backup                                                     \
+      _restore_media                                                           \
+      _restore_plugin                                                          \
+      _restore_theme                                                           \
+      _exit
+    do
+      if [[ -n $HELPER ]]
+        then
+          set -- "$HELPER"
+          break
+        else
+          echo 'Invalid selection, enter the number of a helper in the list'
+      fi
+    done
 
 fi
 
@@ -29,58 +45,249 @@ case $1 in
 
     # It doesn't matter that the credentials are not secure since we're only
     # going to use this user on the developer desktop.
-    wp user create admin admin@localhost.localdomain                           \
+    php /wp-cli.phar                                                           \
+      --allow-root                                                             \
+      user create admin admin@localhost.localdomain                            \
       --role=administrator --user_pass=password
 
-    ;;
+  ;;
 
-  _remove-contact-form-recaptcha-integration)
+  _correct_site_url)
 
-    # Remove Contact Form 7 integration with reCAPTCHA
-
-    wp option patch delete wpcf7 recaptcha
-
-    ;;
-
-  _restore-from-backup)
-
-    echo "Restoring the WordPress installation from the backup to the container"
-    # Find the archive file of website's home folder
-    archive=$(find /backup -name "*.tar.gz")
-    echo "Archive file found at $archive"
-    # Get the full path of the wp_config file from the archive
-    wp_config=$(tar --list --file=$archive --wildcards "*/wp-config.php")
-    echo "wp-config.php found at $wp_config"
-    # Extract all the forward slashes from the full path of the wp_config file
-    slashes="${wp_config//[^\/]}"
-    # Count the components to strip from file names on extraction
-    components="${#slashes}"
-    # Extract from the archive to the WordPress home in the container
-    tar                                                                        \
-      --extract                                                                \
-      --file=$archive                                                          \
-      --directory=/var/www/html                                                \
-      --strip-components=$components                                           \
-      --exclude=$wp_config
-    echo "Extract from the archive completed"
-
-    echo                                                                       \
-      "Change this site's URLs in the WordPress database to match the container"
+    echo "Change the site's URL in the database to match the container URL"
     IFS=':'
     read -a environments <<< "$ENVIRONMENTS"
     for environment in "${environments[@]}"
     do
       echo                                                                     \
         "Replace https://$environment.$DOMAIN with http://$COMPOSE_PROJECT_NAME"
-      wp search-replace --report-changed-only                                  \
+      php /wp-cli.phar --allow-root search-replace --report-changed-only       \
         https://$environment.$DOMAIN http://$COMPOSE_PROJECT_NAME
     done
 
-    ;;
+  ;;
+
+  _export_post)
+
+    cd /posts
+    read -p 'Post name: ' post_name
+
+    # NOTE: The post list requires that the post is published
+    php /wp-cli.phar export                                                    \
+      --allow-root                                                             \
+      --path=/var/www/html/                                                    \
+      --post__in="$(                                                           \
+          php /wp-cli.phar post list                                           \
+            --allow-root                                                       \
+            --path=/var/www/html/                                              \
+            --name=$post_name                                                  \
+            --format=ids                                                       \
+        )"                                                                     \
+      --filename_format=${post_name}.xml
+
+    chown ${UID}:${GID} /posts/${post_name}.xml
+
+  ;;
+
+  _import_post)
+
+    cd /posts
+    read -p 'Post name: ' post_name
+
+    php /wp-cli.phar import                                                    \
+      --allow-root                                                             \
+      --path=/var/www/html/                                                    \
+      ${post_name}.xml                                                         \
+      --authors=skip
+
+    chown ${UID}:${GID} /posts/${post_name}.xml
+
+  ;;
+
+  _install_importer)
+
+    php /wp-cli.phar                                                           \
+      --allow-root                                                             \
+      plugin install wordpress-importer --activate
+
+  ;;
+
+  _remove-contact-form-recaptcha-integration)
+
+    # Remove Contact Form 7 integration with reCAPTCHA
+
+    php /wp-cli.phar option patch delete wpcf7 recaptcha
+
+  ;;
+
+  _restore_media)
+
+    # Find the archive file of website's home folder
+    archive=$(find /backup -name "*.tar.gz")
+    echo "Archive file found at $archive"
+
+    if [[ ! -e /var/www/html/wp-content/uploads ]]
+      then
+        mkdir -p /var/www/html/wp-content/uploads
+    fi
+
+    tar                                                                        \
+      --extract                                                                \
+      --file=$archive                                                          \
+      --directory=/var/www/html/wp-content/uploads                             \
+      --transform="s~.*wp-content/uploads/~~"                                  \
+      --wildcards                                                              \
+      "**wp-content/uploads/**"
+
+    chown -R www-data:www-data /var/www/html/wp-content/uploads/*
+
+  ;;
+
+  _restore_plugin)
+
+    # Find the archive file of website's home folder
+    archive=$(find /backup -name "*.tar.gz")
+    echo "Archive file found at $archive"
+
+    for path in                                                                \
+      $(tar --list --file=$archive --wildcards "**wp-content/plugins/")
+    do
+      regex='wp-content/plugins/([a-z0-9-]+)/$'
+      if [[ $path =~ $regex ]]
+        then
+          if [[ -z "$plugins" ]]
+            then
+              plugins=${BASH_REMATCH[1]}
+            else
+              plugins="$plugins ${BASH_REMATCH[1]}"
+          fi
+      fi
+    done
+
+    echo 'Plugins available'
+    PS3='Plugin to restore? '
+    select plugin in $plugins
+    do
+      if [[ -n $plugin ]]
+        then
+          set -- "$plugin"
+          break
+        else
+          echo 'Invalid selection, enter the number of a plugin in the list'
+      fi
+    done
+
+    tar                                                                        \
+      --extract                                                                \
+      --file=$archive                                                          \
+      --directory=/var/www/html/wp-content/plugins                             \
+      --transform="s~.*wp-content/themes/$plugin/~$plugin/~"                   \
+      --wildcards                                                              \
+      "**wp-content/plugins/$plugin/"
+
+  ;;
+
+  _restore_theme)
+
+    # Find the archive file of website's home folder
+    archive=$(find /backup -name "*.tar.gz")
+    echo "Archive file found at $archive"
+
+    for path in                                                                \
+      $(tar --list --file=$archive --wildcards "**wp-content/themes/")
+    do
+      regex='wp-content/themes/([a-z0-9-]+)/$'
+      if [[ $path =~ $regex ]]
+        then
+          if [[ -z "$themes" ]]
+            then
+              themes=${BASH_REMATCH[1]}
+            else
+              themes="$themes ${BASH_REMATCH[1]}"
+          fi
+      fi
+    done
+
+    echo 'Themes available'
+    PS3='Theme to restore? '
+    select theme in $themes
+    do
+      if [[ -n $theme ]]
+        then
+          set -- "$theme"
+          break
+        else
+          echo 'Invalid selection, enter the number of a theme in the list'
+      fi
+    done
+
+    tar                                                                        \
+      --extract                                                                \
+      --file=$archive                                                          \
+      --directory=/var/www/html/wp-content/themes                              \
+      --transform="s~.*wp-content/themes/$theme/~$theme/~"                     \
+      --wildcards                                                              \
+      "**wp-content/themes/$theme/"
+
+  ;;
+
+  _restore-from-backup)
+
+    echo 'Restoring the WordPress installation from the backup to the container'
+
+    # Find the archive file of website's home folder
+    archive=$(find /backup -name "*.tar.gz")
+    echo "Archive file found at $archive"
+
+    wp_settings=$(tar --list --file=$archive --wildcards "*/wp-settings.php")
+    # Extract all the forward slashes from the full path of the wp_config file
+    slashes="${wp_settings//[^\/]}"
+    # Count the components to strip from file names on extraction
+    components="${#slashes}"
+
+    echo $components
+
+    # If I have used a bacula restore to create the archive, then it will
+    # contain the wp-config.php file. If I have used `scp -rp` instead, then it
+    # won't because of file permissions on the live server. Look to see if this
+    # archive contains the wp-config.php file. Using `grep` rather than a tar
+    # wildcard pattern avoids a bash failure status that halts execution.
+    if [ $(tar --list --file=$archive | grep -F 'wp-config.php') ]
+      then
+
+        wp_config=$(tar --list --file=$archive --wildcards "*/wp-config.php")
+        echo "wp-config.php found at $wp_config"
+        # Extract from the archive to the WordPress home in the container
+        tar                                                                    \
+          --extract                                                            \
+          --file=$archive                                                      \
+          --directory=/var/www/html                                            \
+          --strip-components=$components                                       \
+          --exclude=$wp_config
+
+      else
+
+        echo 'No wp-config.php file found in the archive'
+        tar                                                                    \
+          --extract                                                            \
+          --file=$archive                                                      \
+          --directory=/var/www/html                                            \
+          --strip-components=$components
+
+    fi
+
+    echo 'Extract from the archive completed'
+
+  ;;
+
+  _exit)
+
+  ;;
 
   *)
 
-    wp $@
-    ;;
+    php /wp-cli.phar --allow-root $@
+
+  ;;
 
 esac
